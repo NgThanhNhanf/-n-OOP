f638e81f4c7e8d28d9b3c3f4d247c3b7cf1bc779
Nguyen Hoang Anh <nha261105@gmail.com>
2024-10-19 07:07:44 +0700

    update lai package payment

--- HoangAnh-OOP/src/Main.java
+++ HoangAnh-OOP/src/Main.java
@@ -2,15 +2,26 @@ import Model.Accessory;
 import Model.Clothing;
 import Model.Product;
 import Model.Shoes;
+import Payment.CashPayment;
+import Payment.CardPayment;
+import Payment.WalletPayment;
+import Payment.Bill;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Product item = new Product();
-        Product item1 = new Accessory();
-        Product item2 = new Clothing();
-        Product item3 = new Shoes();
-        //item.displayInfor();
-        //item1.displayInfor();
-        item2.displayInfor();
+        System.out.println("Thanh toan bang tien mat!");
+        CashPayment cash = new CashPayment();
+        cash.pay(200000,150000);
+
+        System.out.println("Thanh toan bang the ngan hang!");
+        CardPayment card = new CardPayment("1234567891234567",new Date());
+        card.pay(200000,150000);
+
+
+        System.out.println("Thanh toan bang vi dien tu!");
+        WalletPayment wallet = new WalletPayment("0985922611");
+        wallet.pay(200000,150000);
     }
 }
--- HoangAnh-OOP/src/Payment/WalletPayment.java
+++ HoangAnh-OOP/src/Payment/WalletPayment.java
@@ -3,41 +3,47 @@ package Payment;
 public class WalletPayment implements PayMent{
     private String soDienThoai;
     private double balance;
+    public WalletPayment(String soDienThoai){
+        this.soDienThoai = soDienThoai;
+    }
     @Override
     public void pay(double amount,double priceItem) {
         if(!isValid(soDienThoai)){
             System.out.println("So tien can thanh toan: " + priceItem);
             if(amount > priceItem){
                 balance = amount - priceItem;
-            }else balance = 0;
+                System.out.println("Thanh toan thanh cong!");
+                bill(amount,priceItem);
+            }else System.out.println("Thanh toan khong thanh cong");
         }else{
             System.out.println("So dien thoai khong hop le!");
             return;
         }
     }
     public double getBalance(){
-        return balance  = 0 ;
+        return balance;
     }
     @Override
     public void refund(double amount) {
-        System.out.println("Hoan tra khong can thiet!");
+        if (balance > 0) {
+            System.out.println("Hoàn trả: " + balance);
+        } else {
+            System.out.println("Không có số dư để hoàn trả.");
+        }
     }
 
     @Override
     public boolean isValid(String details) {
-        for (int i = 0; i < details.length(); i++) {
-            if (details.charAt(i) < 0 && details.charAt(i) > 9) return false;
-            return true;
+        for(char x : details.toCharArray()){
+            if(!Character.isDigit(x)) return false;
         }
-        if (details.length() != 9) return false;
+        if(details.length() != 9) return false;
         return true;
     }
-    @Override
+
     public void bill(double amount,double priceItem) {
-        System.out.println("THANH TOAN BANG VI DIEN TU");
-        System.out.println("-------------Hóa đơn bán hàng--------------");
-        System.out.println("Gía sản phẩm: " + priceItem);
-        System.out.println("Tiền khách đưa: " + amount);
-        System.out.println("Tiền trả lại: " + getBalance());
+        Bill.printBill("Ví điện tử");
+        Bill.printBillDetails(priceItem, amount, getBalance());
+        Bill.printBillFooter();
     }
 }
